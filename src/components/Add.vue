4270f686dd3938f97647399ee4b3097d820fcbe6
unknown <894421269@qq.com>
2020-03-01 15:54:57 +0800

    wills vip

---
+++ src/components/Add.vue
@@ -0,0 +1,78 @@
+<template>
+  <div class="add container">
+    <h1 class="page-header">添加用户</h1>
+    <form @submit="addCustomer">
+      <div class="well">
+        <div class="form-grounp">
+         <br>
+          <input type="text" placeholder="name" class="form-control" v-model="customer.name">
+        </div>
+        <div class="form-grounp">
+         <br>
+          <input type="text" placeholder="phone" class="form-control" v-model="customer.phone">
+        </div>
+        <div class="form-grounp">
+         <br>
+          <input type="text" placeholder="email" class="form-control" v-model="customer.email">
+        </div>
+        <div class="form-grounp">
+         <br>
+          <input type="text" placeholder="education" class="form-control" v-model="customer.education">
+        </div>
+        <div class="form-grounp">
+         <br>
+          <input type="text" placeholder="school" class="form-control" v-model="customer.school">
+        </div>
+        <div class="form-grounp">
+         <br>
+          <input type="text" placeholder="profession" class="form-control" v-model="customer.profession">
+        </div>
+        <div class="form-grounp">
+         <br>
+          <textarea placeholder=" your profile" class="form-control" cols="30" rows="10" v-model="customer.profile"></textarea>
+        </div>
+        <br>
+        <button type="submit" class="btn btn-primary">添加</button>
+      </div>
+    </form>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'add',
+  data () {
+    return {
+     customer:{}
+    }
+  },
+  methods:{
+    addCustomer(e){
+        console.log(this.customer)
+        e.preventDefault()
+        if(!this.customer.name || !this.customer.phone || !this.customer.email){
+          alert("请添加相对应信息")
+        }else{
+          e.preventDefault();
+          let newCustomer={
+            name:this.customer.name,
+            phone:this.customer.phone,
+            email:this.customer.email,
+            education:this.customer.education,
+            school:this.customer.school,
+            profession:this.customer.profession,
+            profile:this.customer.profile
+        }
+        this.$http.post("http://localhost:3000/users",newCustomer).then((response)=>{
+          this.$router.push({path:"/",query:{alert:"添加成功"}})
+        })
+      }
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+</style>
---
+++ src/components/Customers.vue
@@ -0,0 +1,53 @@
+<template>
+  <div class="customers container">
+    
+    <Alert v-if="alert" :message="alert"></Alert>
+    <h1 class="page-header">会员信息</h1>
+    <table class="table table-striped">
+      <thead>
+        <tr>
+          <th>姓名</th>
+          <th>邮箱</th>
+          <th>电话</th>
+          <th></th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr v-for="customer in customers">
+          <th>{{customer.name}}</th>
+          <th>{{customer.eamil}}</th>
+          <th>{{customer.phone}}</th>
+          <th></th>
+        </tr>
+      </tbody>
+    </table>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'customers',
+  data () {
+    return {
+     customers:[],
+     alert:""
+    }
+  },
+  methods:{
+    fetchCustomer(){
+      this.$http.get("http://localhost:3000/users").then(function(response){
+        console.log(response.body)
+        this.customers=response.body
+      })
+    }
+  },
+  created(){
+    this.fetchCustomer()
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+</style>
